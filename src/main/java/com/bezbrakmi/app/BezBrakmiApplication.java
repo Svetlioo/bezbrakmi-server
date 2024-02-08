65c357d2f1f9423000aa29ead66bcbae7c3925ce
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2024-02-08 19:42:38 +0200

    Remove unused imports

--- src/main/java/com/bezbrakmi/app/BezBrakmiApplication.java
+++ src/main/java/com/bezbrakmi/app/BezBrakmiApplication.java
@@ -1,7 +1,20 @@
 package com.bezbrakmi.app;
 
+import com.bezbrakmi.app.entity.CarListing;
+import com.bezbrakmi.app.entity.CarOption;
+import com.bezbrakmi.app.model.BodyType;
+import com.bezbrakmi.app.model.CarBrand;
+import com.bezbrakmi.app.model.OptionCategory;
+import com.bezbrakmi.app.repository.CarListingRepository;
+import com.bezbrakmi.app.service.CarListingService;
+import com.bezbrakmi.app.service.CarListingServiceImpl;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class BezBrakmiApplication {
